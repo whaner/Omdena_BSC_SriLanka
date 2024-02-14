97aaf456a732a6c476ae477bb533342a517f0bed
Whaner Endo <54806151+whaner@users.noreply.github.com>
2024-02-13 22:18:13 -0300

    Ceylon Today's scripts and dataset

--- ceylontoday/sundayceilo_get_url.py
+++ ceylontoday/sundayceilo_get_url.py
@@ -4,16 +4,20 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # Base URL of the website to scrape
-base_url = "https://ceylontoday.lk/category/ceylon-today-sunday/news-ceylon-today-sunday/page/" #Ceylon Today – Sunday
+#base_url = "https://ceylontoday.lk/category/ceylon-today-sunday/news-ceylon-today-sunday/page/" #Ceylon Today – Sunday
+base_url = "https://ceylontoday.lk/category/ceylon-today-daily/news/page/" #Ceylon Today – Sunday
 
 # Initialize an empty list to store article data
 articles_data = []
-
-# Iterate over the first 120 pages
-for page in range(1, 163):
+pagina = 0
+# Iterate over the pages
+for page in range(1, 1193):
+    pagina = pagina+1
+    print(pagina)
     url = f"{base_url}{page}/"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
+
 
     # Find all article entries on the page
     articles = soup.find_all('h3', class_='entry-title td-module-title')
@@ -25,4 +29,4 @@ for page in range(1, 163):
 df = pd.DataFrame(articles_data, columns=['url'])
 
 # Save the DataFrame to a CSV file
-df.to_csv('ceylon_today_sunday_urls.csv', index=False, encoding='utf-8', sep=',')
+df.to_csv('ceylon_today_daily_urls.csv', index=False, encoding='utf-8', sep=',')
